559126d7f3865c98744f046c10853a5b6bf8b25f
Ravi Riley <raviriley@gmail.com>
2025-03-08 20:20:57 -0800

    Add command bar

--- centurion/app/page.tsx
+++ centurion/app/page.tsx
@@ -1,7 +1,70 @@
+import {
+  Banknote,
+  Calculator,
+  Calendar,
+  CloudUpload,
+  CreditCard,
+  HandCoins,
+  Replace,
+  Settings,
+  Smile,
+} from "lucide-react";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      hello world
+    <div className="flex flex-col gap-4">
+      <div className="text-xl font-bold">
+        interact with Flare primitives using natural language
+      </div>
+      <Command className="rounded-lg border shadow-md md:min-w-[450px]">
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Blockchain Actions">
+            <CommandItem>
+              <Replace />
+              <span>Swap Tokens</span>
+            </CommandItem>
+            <CommandItem>
+              <CloudUpload />
+              <span>Store Data</span>
+            </CommandItem>
+            <CommandItem>
+              <HandCoins />
+              <span>Lend Assets</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="More Options">
+            <CommandItem>
+              <Banknote />
+              <span>Borrow Assets</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Smile />
+              <span>Explore Blockchain</span>
+              <CommandShortcut>⌘E</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
     </div>
   );
 }
